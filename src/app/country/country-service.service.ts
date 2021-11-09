3049ee51e573e6afa835936636382b7e5e6d3bfb
AksheenC <aksheen.galundia@carqsconsulting.com>
2021-11-09 10:25:35 +0530

    changes for API changes

--- src/app/country/country-service.service.ts
+++ src/app/country/country-service.service.ts
@@ -10,19 +10,19 @@ export class CountryService {
   constructor(private backend: BackendService) { }
 
   searchAllCountry(): Observable<any> {
-    return this.backend.getHttp('https://restcountries.eu' + '/rest/v2/all')
+    return this.backend.getHttp('https://restcountries.com' + '/v2/all')
   }
 
   getCountryData(input: string): Observable<any> {
-    return this.backend.getHttp('https://restcountries.eu' + '/rest/v2/name/' + input);
+    return this.backend.getHttp('https://restcountries.com' + '/v2/name/' + input);
   }
 
   getCountryDataForChart(): Observable<any> {
-    const request1 = this.backend.getHttp('https://restcountries.eu' + '/rest/v2/name/Australia');
-    const request2 = this.backend.getHttp('https://restcountries.eu' + '/rest/v2/name/germany');
-    const request3 = this.backend.getHttp('https://restcountries.eu' + '/rest/v2/name/russia');
-    const request4 = this.backend.getHttp('https://restcountries.eu' + '/rest/v2/name/spain');
-    const request5 = this.backend.getHttp('https://restcountries.eu' + '/rest/v2/name/pakistan');
+    const request1 = this.backend.getHttp('https://restcountries.com' + '/v2/name/Australia');
+    const request2 = this.backend.getHttp('https://restcountries.com' + '/v2/name/germany');
+    const request3 = this.backend.getHttp('https://restcountries.com' + '/v2/name/russia');
+    const request4 = this.backend.getHttp('https://restcountries.com' + '/v2/name/spain');
+    const request5 = this.backend.getHttp('https://restcountries.com' + '/v2/name/pakistan');
     return forkJoin([request1, request2, request3, request4, request5]);
   }
 }
